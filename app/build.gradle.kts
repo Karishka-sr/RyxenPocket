c74d5242eac4eb1440cbe0815dc007e3ebf36cfb
Karishka-sr <volkod221top@gmail.com>
2026-09-21 20:47:36 +0700

    Change Compose BOM version to 2026.06.00
    Updated Compose BOM version in dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2026.09.00"))
+    implementation(platform("androidx.compose:compose-bom:2026.06.00"))
     implementation("androidx.activity:activity-compose:1.12.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
